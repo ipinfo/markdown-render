0bcc1e59ccf76acb2087106412b4f2845f37725a
Matthew <mattmuelle@gmail.com>
2017-01-13 10:31:54 +0700

    Squash history

---
+++ index.js
@@ -0,0 +1,73 @@
+/*
+ * Module dependencies
+ */
+
+var highlight = require('highlight.js')
+var remarkable = require('remarkable')
+var toc = require('markdown-toc')
+
+/**
+ * Export render
+ */
+
+module.exports = render
+
+/**
+ * Mappings between remarkable and highlight.js
+ */
+
+var language = {
+  'js': 'javascript',
+  'html': 'xml',
+  'shell': 'bash'
+}
+
+/**
+ * Highlight configuration
+ */
+
+highlight.configure({
+  tabReplace: '  '
+})
+
+/**
+ * Configure MD
+ */
+
+var md = new remarkable.Remarkable({
+  html: true,        // Enable HTML tags in source
+  xhtmlOut: true,        // Use '/' to close single tags (<br />)
+  breaks: true,        // Convert '\n' in paragraphs into <br>
+  langPrefix: 'lang ',       // CSS language prefix for fenced blocks
+  highlight: function (code, lang) {
+     // differences between remarkable and highlight.js
+    lang = (language[lang]) ? language[lang] : lang
+
+     // Let's not let syntax highlighting kill anything
+    try {
+      return lang
+         ? highlight.highlight(code, {language: lang}).value
+         : highlight.highlightAuto(code).value
+    } catch (e) {}
+
+    return ''
+  }
+})
+
+/**
+ * Add in header anchors
+ */
+
+md.use(function (remarkable) {
+  remarkable.renderer.rules.heading_open = function (tokens, idx) {
+    return '<h' + tokens[idx].hLevel + ' id=' + toc.slugify(tokens[idx + 1].content) + '>'
+  }
+})
+
+/**
+ * Export `render`
+ */
+
+function render (src) {
+  return md.render(src)
+}
